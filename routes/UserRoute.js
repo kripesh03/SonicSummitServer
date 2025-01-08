570e597c0bc86911e7c533de29e466e0d29bf1e8
kripesh03 <kripeshpoudel9842@gmail.com>
2025-01-08 14:03:38 +0545

    user validations added

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const { findAll, save, findById, deleteById, updateById } = require("../controller/UserController");
+const UserValidation = require("../validations/UserValidation");
 const router = express.Router();
 
-router.get("/", findAll); // Get all users
-router.post("/", save); // Save a new user
-router.get("/:id", findById); // Get a user by ID
-router.delete("/:id", deleteById); // Delete a user by ID
-router.put("/:id", updateById); // Update a user by ID (use PUT for full update)
+// Get all users
+router.get("/", findAll);
+
+// Save a new user (Validate user data before saving)
+router.post("/", UserValidation, save);
+
+// Get a user by ID
+router.get("/:id", findById);
+
+// Delete a user by ID
+router.delete("/:id", deleteById);
+
+// Update a user by ID (Validate user data before updating)
+router.put("/:id", UserValidation, updateById);
 
 module.exports = router;
